6e25a509693532bcfc289c37271d3183740b3dcb
kmsaleh <khaledmohammedziedan@gmail.com>
2023-02-14 17:05:15 +0200

    modify

--- src/main/java/com/example/Greeting/Servicea/rest/GreetingControllerFiegn.java
+++ src/main/java/com/example/Greeting/Servicea/rest/GreetingControllerFiegn.java
@@ -1,6 +1,7 @@
 package com.example.Greeting.Servicea.rest;
 
 import com.example.Greeting.Servicea.client.GreetingClient;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +11,31 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greetingfeign")
 public class GreetingControllerFiegn {
     private final GreetingClient greetingClient;
+    private final CircuitBreakerFactory circuitBreakerFactory;
 
-    public GreetingControllerFiegn(GreetingClient greetingClient) {
+    public GreetingControllerFiegn(GreetingClient greetingClient, CircuitBreakerFactory circuitBreakerFactory) {
         this.greetingClient = greetingClient;
+        this.circuitBreakerFactory = circuitBreakerFactory;
     }
 
     @GetMapping("/{id}")
     public String greeting(@PathVariable String id) {
+        return circuitBreakerFactory.create("hello-service").run( ()->callServiceB(id),ex->defaultgreeting());
+    }
+
+    private String callServiceB(String id) {
+        if (id.equals("1000")) {
+            try {
+                Thread.sleep(100000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
         String userName = greetingClient.userGreeting(id);
         return "Hello " + userName;
+    }
+
+    private String defaultgreeting() {
+        return "Hello";
     }
 }
--- src/main/java/com/example/Greeting/Servicea/rest/GreetingControllerRestTemplate.java
+++ src/main/java/com/example/Greeting/Servicea/rest/GreetingControllerRestTemplate.java
@@ -1,5 +1,6 @@
 package com.example.Greeting.Servicea.rest;
 
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +11,36 @@ import org.springframework.web.client.RestTemplate;
 @RequestMapping("/greeting")
 public class GreetingControllerRestTemplate {
     private final RestTemplate restTemplate;
+    private final CircuitBreakerFactory circuitBreakerFactory;
 
-    public GreetingControllerRestTemplate(RestTemplate restTemplate) {
+    public GreetingControllerRestTemplate(RestTemplate restTemplate, CircuitBreakerFactory circuitBreakerFactory) {
         this.restTemplate = restTemplate;
+        this.circuitBreakerFactory = circuitBreakerFactory;
     }
 
     @GetMapping("/{id}")
-    public String greeting(@PathVariable String id) {
+    public String greetingUserName(@PathVariable String id) {
         String userName = restTemplate.getForObject("http://userManagementService/users/{id}", String.class, id);
         return "Hello " + userName;
+    }
+
+    @GetMapping("name/{name}")
+    public String greetingName(@PathVariable String name) {
+        return circuitBreakerFactory.create("hello-internal").run(() -> sayHello(name), ex -> defaultHello());
+    }
+
+    private String sayHello(String userName) {
+        if (userName.equals("Khaled")) {
+            try {
+                Thread.sleep(100000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return "Hello " + userName;
+    }
+
+    private String defaultHello() {
+        return "Hello";
     }
 }
